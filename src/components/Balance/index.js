b5fd544223a3e9e2b9cdf80b6dddd7f2583b8f35
rochac2lee <cleberli.rocha@gmail.com>
2023-09-20 19:48:46 -0300

    Criando componente de entradas e saídas

---
+++ src/components/Balance/index.js
@@ -0,0 +1,79 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { MotiView } from 'moti';
+
+export default function Balance({ balance, expenses }) {
+    return (
+        <MotiView 
+            style={styles.container}
+            from={{
+                rotateX: '-100deg',
+                opacity: 0
+            }}
+            animate={{
+                rotateX: '0deg',
+                opacity: 1
+            }}
+            transition={{
+                type: 'timing',
+                delay: 300,
+                duration: 900
+            }}
+        >
+            <View style={styles.item}>
+                <Text style={styles.itemTitle}>Saldo</Text>
+                <View style={styles.content}>
+                    <Text style={styles.currentSymbol}>R$</Text>
+                    <Text style={styles.balance}>{balance}</Text>
+                </View>
+            </View>
+            <View style={styles.item}>
+                <Text style={styles.itemTitle}>Gastos</Text>
+                <View style={styles.content}>
+                    <Text style={styles.currentSymbol}>R$</Text>
+                    <Text style={styles.expenses}>{expenses}</Text>
+                </View>
+            </View>
+        </MotiView>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#fff',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        paddingStart: 18,
+        paddingEnd: 18,
+        marginTop: -24,
+        marginStart: 14,
+        marginEnd: 14,
+        borderRadius: 4,
+        paddingTop: 22,
+        paddingBottom: 22,
+        zIndex: 99
+    },
+    item: {
+
+    },
+    content: {
+        flexDirection: 'row',
+        alignItems: 'center'
+    },
+    itemTitle: {
+        fontSize: 20,
+        color: '#8000ff'
+    },
+    currentSymbol: {
+        color: '#dadada',
+        marginRight: 6
+    },
+    balance: {
+        fontSize: 22,
+        color: '#2ecc71'
+    },
+    expenses: {
+        fontSize: 22,
+        color: '#e74c3c'
+    }
+})
